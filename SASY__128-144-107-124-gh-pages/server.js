df73795f98fd08015c87cdee4a8c40b75f79cb81
insane1304 <iit2019124@iiita.ac.in>
2021-03-12 19:04:06 +0530

    User registration use case Completed

--- SASY__128-144-107-124-gh-pages/server.js
+++ SASY__128-144-107-124-gh-pages/server.js
@@ -11,14 +11,24 @@ app.use(express.static("public"));
 
 mongoose.connect("mongodb://localhost:27017/todo",{useNewUrlParser:true});
 
-const adminsSchema={
+const adminsSchema=new mongoose.Schema ({
   name:String,
   id:String,
   pass:String
-};
+});
+
+const visitorsSchema=new mongoose.Schema ({
+  name:String,
+  sex:String,
+  address:String,
+  email:String,
+  mobile:String,
+  aadhar:String,
+  pass:String,
+});
 
 const Admin=mongoose.model("Admin",adminsSchema);
-
+const Visitor=mongoose.model("Visitor",visitorsSchema);
 // const admin1=new Admin({
 //   name:"yash",
 //   id:"1",
@@ -51,25 +61,79 @@ app.get("/",function(req,res){
   res.sendfile(__dirname+"/rlex-login.html");
 });
 
+app.get("/rlex-signup.html",function(req,res){
+  res.sendfile(__dirname+"/rlex-signup.html");
+});
+
 app.post("/",function(req,res)
 {
   var email=req.body.myemail;
   var pass=req.body.password;
   // console.log(req.body["password"]);
-  Admin.find({id:email},function(err,check){
+  Admin.findOne({id:email},function(err,check){
     if(err)
     {
-      res.send("INVALID ID");
+      console.log(err);
     }
     else{
-      if(check[0].pass==pass)
-      res.send("Login Success");
-      else
-      res.send("Invalid pass");
+      if(check)
+      {
+        if(check.pass==pass)
+        res.send("Login Success");
+        else
+        res.send("Invalid pass");
+      }
     }
-
-
   })
+  Visitor.findOne({name:email},function(err,check){
+    if(err)
+    {
+      console.log(err);
+    }
+    else{
+      if(check)
+      {
+        if(check.pass==pass)
+        res.send("Login Success");
+        else
+        res.send("Invalid pass");
+      }
+    }
+  })
+
+  // if(Admin.findOne({id:email})==null)
+  res.send("Invalid ID");
+
+});
+
+app.post("/rlex-signup.html",function(req,res)
+{
+  var vName=req.body.name;
+  var vRadiomale=req.body.radiomale;
+  var vRadiofemale=req.body.radiofemale;
+  var vAddress=req.body.address;
+  var vEmail=req.body.email;
+  var vMobile=req.body.mobile;
+  var vAadhar=req.body.aadhar
+  var vPass=req.body.pass;
+  var vSex="";
+  if( vRadiofemale=="on")
+  vSex="Male";
+  else
+  sex="Female";
+
+  const visitor=new Visitor({
+    name: vName,
+    sex: vSex,
+    address: vAddress,
+    email: vEmail,
+    mobile: vMobile,
+    aadhar: vAadhar,
+    pass: vPass,
+  });
+
+ visitor.save();
+  res.send("Registration Success Proceed to login");
 
 
   // res.redirect("/");
