e6db30b3fc1e4c4bb9e4124ff65d8bd25d0b45f4
Aditya Kumar <59906496+theadityakr@users.noreply.github.com>
2021-03-13 15:53:03 +0530

    javascript for admin page

---
+++ SASY__128-144-107-124-gh-pages/js/admin.js
@@ -0,0 +1,45 @@
+//Get the button:
+mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+} 
+
+function myFunction(x) {
+  x.classList.toggle("change");
+} 
+
+function myFunction() {
+   var element = document.body;
+   element.classList.toggle("dark-mode");
+}
+
+function search_user_id(){
+
+var x= document.getElementById("user_id_input").value;
+
+//search for userid in database  and print the detaisl in the place
+
+}
+
+function extend_admin(){
+
+
+var x=document.getElementById("leaving_time").value;
+console.log(x);
+//replace replace leaving time with it 
+
+}
