ef2c3c33b1defb11505060162fac6cc45fc1b011
insane1304 <iit2019124@iiita.ac.in>
2021-04-07 15:22:26 +0530

    Dynamic home logim

--- SASY__128-144-107-124-gh-pages/home.js
+++ SASY__128-144-107-124-gh-pages/home.js
@@ -1,10 +1,32 @@
 
 module.exports = function(app){
   app.get("/",function(req,res){
-    res.render("home.ejs");
+    if(req.isAuthenticated()){
+      if(req.user.username[0]=='a')
+      {
+        res.render("home.ejs",{user:"admin"});
+      }
+      else
+      {
+        res.render("home.ejs",{user:"visitor"});
+      }
+    }
+    else
+    res.render("home.ejs",{user:"nil"});
   });
   app.get("/home",function(req,res){
-    res.redirect("/");
+    if(req.isAuthenticated()){
+      if(req.user.username[0]=='a')
+      {
+        res.render("home.ejs",{user:"admin"});
+      }
+      else
+      {
+        res.render("home.ejs",{user:"visitor"});
+      }
+    }
+    else
+    res.render("home.ejs",{user:"nil"});
   });
     //other routes..
 }
