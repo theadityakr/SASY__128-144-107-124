2531cd1dc57cb4736f7cddf4212993272a8a1b0b
Saloni128 <67595608+Saloni128@users.noreply.github.com>
2021-05-21 17:23:54 +0530

    final

--- SASY__128-144-107-124-gh-pages/home.js
+++ SASY__128-144-107-124-gh-pages/home.js
@@ -1,32 +1,38 @@
 
 module.exports = function(app){
+
   app.get("/",function(req,res){
+
     if(req.isAuthenticated()){
-      if(req.user.username[0]=='a')
-      {
+
+      if(req.user.username[0]=='a'){
         res.render("home.ejs",{user:"admin"});
       }
-      else
-      {
+      else{
         res.render("home.ejs",{user:"visitor"});
       }
     }
+
     else
     res.render("home.ejs",{user:"nil"});
+
   });
+
   app.get("/home",function(req,res){
+
     if(req.isAuthenticated()){
-      if(req.user.username[0]=='a')
-      {
+
+      if(req.user.username[0]=='a'){
         res.render("home.ejs",{user:"admin"});
       }
-      else
-      {
+      else{
         res.render("home.ejs",{user:"visitor"});
       }
     }
+
     else
     res.render("home.ejs",{user:"nil"});
   });
+
     //other routes..
 }
